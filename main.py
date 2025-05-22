4010c6cd25fe4a96e203949d3547dffb4bac4e7f
jepras <jeprasher@gmail.com>
2025-05-22 15:22:30 +0200

    added plotly

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
 from get_song_info import get_artist_name, get_sampled_songs
 from search import search_song
+from whosampled.utils.graph_utils import build_graph, plot_graph
+
+import plotly.graph_objects as go
+import networkx as nx
+
 
 def main():
     # artist = get_artist_name(SONG_ID)
     # print("Artist:", artist)
 
-    song_id = search_song("The World Is Yours", artist_name="Nas")
-    print("Song ID:", song_id)
+    # song_id = search_song("The World Is Yours", artist_name="Nas")
+    # print("Song ID:", song_id)
+
+    nodes = ["The World Is Yours", "Sampled Song 1", "Sampled Song 2"]
+    edges = [("The World Is Yours", "Sampled Song 1"), ("The World Is Yours", "Sampled Song 2")]
+
+    G = build_graph(nodes, edges)
+    plot_graph(G)
+    
 
 if __name__ == "__main__":
     main() 
---
+++ whosampled/utils/graph_utils.py
@@ -0,0 +1,56 @@
+import networkx as nx
+import plotly.graph_objects as go
+
+def build_graph(nodes, edges):
+    G = nx.DiGraph()
+    G.add_nodes_from(nodes)
+    G.add_edges_from(edges)
+    return G
+
+def plot_graph(G):
+    # Use a layout to get node positions (spring_layout is a good default)
+    pos = nx.spring_layout(G)
+
+    # Create edge traces
+    edge_x = []
+    edge_y = []
+    for src, dst in G.edges():
+        x0, y0 = pos[src]
+        x1, y1 = pos[dst]
+        edge_x += [x0, x1, None]
+        edge_y += [y0, y1, None]
+
+    edge_trace = go.Scatter(
+        x=edge_x, y=edge_y,
+        line=dict(width=1, color='#888'),
+        hoverinfo='none',
+        mode='lines'
+    )
+
+    # Create node traces
+    node_x = []
+    node_y = []
+    for node in G.nodes():
+        x, y = pos[node]
+        node_x.append(x)
+        node_y.append(y)
+
+    node_trace = go.Scatter(
+        x=node_x, y=node_y,
+        mode='markers+text',
+        text=list(G.nodes()),
+        textposition="bottom center",
+        marker=dict(size=20, color='lightblue'),
+        hoverinfo='text'
+    )
+
+    fig = go.Figure(data=[edge_trace, node_trace],
+                    layout=go.Layout(
+                        showlegend=False,
+                        hovermode='closest',
+                        margin=dict(b=20, l=5, r=5, t=40),
+                        xaxis=dict(showgrid=False, zeroline=False, showticklabels=False),
+                        yaxis=dict(showgrid=False, zeroline=False, showticklabels=False)
+                    ))
+
+    fig.show()
